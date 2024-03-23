bb2456831730cdee8293b0439fc0ae03de2859e7
el-sapo <federico.lagarmilla@gmail.com>
2024-03-23 02:45:07 -0300

    add vercel analytics vercel redirect problems

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import { WagmiProvider, createConfig } from 'wagmi';
 import { sepolia } from 'wagmi/chains'; // add base
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ConnectKitProvider, getDefaultConfig, ConnectKitButton } from 'connectkit';
+import { Analytics } from "@vercel/analytics/react"
 
 const config = createConfig(
   getDefaultConfig({
@@ -25,6 +26,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <ConnectKitProvider>
         <ConnectKitButton />
         <App />
+        <Analytics />
         </ConnectKitProvider>
       </QueryClientProvider>
     </WagmiProvider>  
